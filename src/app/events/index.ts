84df3605c2182bd7946dd3516c98205b565d6951
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-29 16:00:06 +0600

    Index ts  :bulb:

--- src/app/events/index.ts
+++ src/app/events/index.ts
@@ -1,7 +1,7 @@
 import initAcademicDepartmentEvents from "../modules/academicDepartment/academicDepartment.events";
 import initAcademicFacultyEvents from "../modules/academicFaculty/academicFaculty.events";
 import initAcademicSemesterEvents from "../modules/academicSemester/academicSemester.event";
-
+// index ts
 const subscribeToEvents = () => {
     initAcademicSemesterEvents();
     initAcademicDepartmentEvents();
